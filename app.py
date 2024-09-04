91a4725345a3b3e8d2eb6cba76ef5eef1103dc58
Luis <luis.manuel.quiroz@gmail.com>
2024-09-03 20:46:38 -0600

    agregue la logica del histograma a la web

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-import os
-import pandas
-import plotly.express
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv("datasets/vehicles_us.csv")
+hist_button = st.button('construir histograma')
 
 
-print(f'my working directory is {os.getcwd()}')
+if hist_button:
+    st.write('CReación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig  = px.histogram(car_data, x="odometer")
+    st.plotly_chart(fig, use_container_width = True)
+    
